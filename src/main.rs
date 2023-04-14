c626c8dab6479a3264bc136e84e03f3d4de35e8b
Guillaume Delepoulle <delepoulle@telecom-paris.fr>
2023-04-14 15:40:20 +0200

    feat(main.rs): get python file path and python binary in struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct PythonScript {
+    python_file_path: String,
+    python_env_binary_path: String,
+}
+
+impl PythonScript {
+    fn run(&self) {
+        println!(
+            "Running file {} with binary {}!",
+            self.python_file_path, self.python_env_binary_path
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, adding pre-commits!");
+    let python_script = PythonScript {
+        python_file_path: std::env::args().nth(1).expect("No python file given"),
+        python_env_binary_path: std::env::args().nth(2).expect("No python binary was given"),
+    };
+    python_script.run()
 }
