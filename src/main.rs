60082f0df5e9c4bd1b57902ac49d03c880c161df
gdelepoulle <92gdelepoulle@gmail.com>
2023-05-08 18:36:14 +0200

    feat: Run a python file given as input (#5)
    * feat: Adding pre-commit and conventionnal commits
    
    * feat(.pre-commit-config.yaml): add formating clippy and commitlint
    
    * feat(main.rs): get python file path and python binary in struct
    
    * feat(main.rs): running bash command to exectute python file
    
    * tests(python_script): add first tests
    
    ---------
    
    Co-authored-by: Guillaume Delepoulle <delepoulle@telecom-paris.fr>
    Co-authored-by: Emilio DE SOUSA <emilio.desousa@sicara.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, adding pre-commits!");
+use std::process::Command;
+use std::process::ExitStatus;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct PythonScript {
+    python_file_path: String,
+    python_env_binary_path: String,
+}
+
+impl PythonScript {
+    fn run(&self) -> ExitStatus {
+        let output = Command::new(&self.python_env_binary_path)
+            .arg(&self.python_file_path)
+            .output()
+            .expect("failed to execute process");
+
+        println!("Execution done.");
+        println!("Status code is: \n{}", output.status);
+        output.status
+    }
+}
+
+fn main() -> ExitStatus {
+    let python_script = PythonScript::parse();
+    python_script.run()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_python_script_run_success() {
+        let python_script = PythonScript {
+            python_file_path: "tests/success.py".to_string(),
+            python_env_binary_path: "/usr/bin/python3".to_string(),
+        };
+
+        let return_code = python_script.run();
+        const SUCCESS_CODE: i32 = 0;
+
+        assert_eq!(return_code.code(), Some(SUCCESS_CODE));
+    }
+    #[test]
+    fn test_python_script_run_failing() {
+        let python_script = PythonScript {
+            python_file_path: "tests/error.py".to_string(),
+            python_env_binary_path: "/usr/bin/python3".to_string(),
+        };
+
+        let return_code = python_script.run();
+        const FAILING_CODE: i32 = 1;
+
+        assert_eq!(return_code.code(), Some(FAILING_CODE));
+    }
 }
